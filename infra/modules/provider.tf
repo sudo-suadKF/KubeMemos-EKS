20042c29f7d1491f1b06a560cfa0a8f8a01ce5cd
sudo-suadKF <suadfehric@outlook.com>
2026-02-21 12:48:43 +0100

    updt: added infracost in gitignore, add: backend s3 in provider.tf for terraform state and state locking

--- infra/modules/provider.tf
+++ infra/modules/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "6.33.0"
     }
   }
+
+  backend "s3" {
+    bucket       = "eks-bootstrap-bucket-for-terraform-state"
+    key          = "terraform.tfstate"
+    use_lockfile = true
+    region       = "eu-west-2"
+  }
 }
 
 provider "aws" {
