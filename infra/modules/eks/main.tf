cf9192c98522c72f07a3c9c5f3e74a6c6dbf8d1b
sudo-suadKF <suadfehric@outlook.com>
2026-02-23 00:40:35 +0100

    add: eks main.tf and variable.tf, vpc outputs.tf, change: private and public subnets resource

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -1,18 +1,18 @@
 resource "aws_eks_cluster" "my-eks-cluster" {
-  name = "my-eks-cluster"
+  name     = "my-eks-cluster"
   role_arn = ""
-  version = "1.35"
+  version  = "1.35"
 
   access_config {
-    authentication_mode = "API"
+    authentication_mode                         = "API"
     bootstrap_cluster_creator_admin_permissions = true
   }
 
   vpc_config {
-    subnet_ids = var.public-subs-id
+    subnet_ids              = var.public-subs-id
     endpoint_private_access = true
-    endpoint_public_access = true
-    public_access_cidrs = [ "0.0.0.0/0" ]
+    endpoint_public_access  = true
+    public_access_cidrs     = ["0.0.0.0/0"]
   }
 
 
@@ -20,7 +20,7 @@ resource "aws_eks_cluster" "my-eks-cluster" {
 
 resource "aws_eks_addon" "my-addons" {
   count = length(var.addons)
-  
+
   cluster_name = aws_eks_cluster.my-eks-cluster.name
   addon_name   = var.addons[count.index]
 
--- infra/modules/eks/variables.tf
+++ infra/modules/eks/variables.tf
@@ -1,10 +1,10 @@
 variable "public-subs-id" {
   description = "Contains public subnets IDs"
-  type = list(string)
+  type        = list(string)
 }
 
 variable "addons" {
   description = "Contains addons for cluster"
-  type = list(string)
-  default = [ "vpc-cni", "coredns", "kube-proxy", "eks-pod-identity-agent" ]
+  type        = list(string)
+  default     = ["vpc-cni", "coredns", "kube-proxy", "eks-pod-identity-agent"]
 }
