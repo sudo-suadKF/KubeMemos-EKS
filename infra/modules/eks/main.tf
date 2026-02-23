271f52533f55b43d1e360c8becccba35d22b0ba8
sudo-suadKF <suadfehric@outlook.com>
2026-02-23 02:16:39 +0100

    add: node group IAM role and attached policies to it

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -55,7 +55,7 @@ resource "aws_iam_role_policy_attachment" "policy-role-attach" {
 resource "aws_eks_node_group" "my-node-group" {
   cluster_name = aws_eks_cluster.my-eks-cluster.name
   node_group_name = "my-node-group"
-  node_role_arn = ""
+  node_role_arn = aws_iam_role.node-group-role.arn
   subnet_ids = var.private-subs-id
 
   scaling_config {
@@ -67,4 +67,33 @@ resource "aws_eks_node_group" "my-node-group" {
   update_config {
     max_unavailable = 1
   }
+}
+
+resource "aws_iam_role" "node-group-role" {
+  name = "eks-node-group-role"
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "worker-node-ec2-attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node-group-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "networking-pod-ip-attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node-group-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "worker-node-ecr-attach" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node-group-role.name
 }
