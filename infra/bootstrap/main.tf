d34987066e5e4d99c8554d2bc30be38e72bd1ef5
sudo-suadKF <suadfehric@outlook.com>
2026-02-25 12:07:36 +0100

    add: kms key encryption for ecr repo, variables for aws account id got declared

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -2,6 +2,7 @@ module "ecr" {
   source                  = "./modules/ecr"
   ecr-repo-name           = var.ecr-repo-name
   ecr-repo-tag-mutability = var.ecr-repo-tag-mutability
+  aws-account-id          = var.aws-account-id
 }
 
 module "s3" {
