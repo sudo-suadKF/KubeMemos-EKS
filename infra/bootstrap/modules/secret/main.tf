e8cf3036bbba9f04952e4df0a89357652317ef96
sudo-suadKF <suadfehric@outlook.com>
2026-06-20 21:11:01 +0200

    change: setting up secret in secrets manager, removing rotation

--- infra/bootstrap/modules/secret/main.tf
+++ infra/bootstrap/modules/secret/main.tf
@@ -2,26 +2,18 @@ resource "aws_secretsmanager_secret" "rds-credentials" {
   name = "production/rds/credentials"
 }
 
-resource "random_password" "rds-password" {
-  length = 32
-  special = false
-}
-
 resource "aws_secretsmanager_secret_version" "rds-credentials" {
   secret_id = aws_secretsmanager_secret.rds-credentials.id
   secret_string = jsonencode({
     username            = "memos-user"
-    password            = random_password.rds-password.result
   })
 }
 
 resource "aws_secretsmanager_secret_rotation" "rds-credentials" {
   secret_id = aws_secretsmanager_secret.rds-credentials.id
-  
+  rotation_lambda_arn = 
   
   rotation_rules {
     automatically_after_days = 30
   }
-
-
 }
