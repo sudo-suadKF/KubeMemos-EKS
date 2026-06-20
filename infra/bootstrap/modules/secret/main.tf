a8b7c24bf98bff0ec7025d71f71eed14c657a7ab
sudo-suadKF <suadfehric@outlook.com>
2026-06-20 21:17:53 +0200

    add: random password, storing in secret

--- infra/bootstrap/modules/secret/main.tf
+++ infra/bootstrap/modules/secret/main.tf
@@ -1,3 +1,14 @@
+resource "random_password" "rds-password" {
+  length           = 32
+  special          = true
+  override_special = "!#$%^&*()-_=+[]{}|:,.<>?"
+  # Avoid characters that cause issues in connection strings
+  min_lower   = 4
+  min_upper   = 4
+  min_numeric = 4
+  min_special = 2
+}
+
 resource "aws_secretsmanager_secret" "rds-credentials" {
   name = "production/rds/credentials"
 }
@@ -6,6 +17,6 @@ resource "aws_secretsmanager_secret_version" "rds-credentials" {
   secret_id = aws_secretsmanager_secret.rds-credentials.id
   secret_string = jsonencode({
     username            = "memos-user"
-    password = 
+    password = random_password.rds-password.result
   })
 }
