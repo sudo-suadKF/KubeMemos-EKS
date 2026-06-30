080afcf0ef7d39216ed7efcdf18971a8fd5d3944
sudo-suadKF <suadfehric@outlook.com>
2026-06-30 17:40:52 +0200

    change: moved password and version resource to passwd rotation folder, add: secret rotation resource and data block secret

---
+++ infra/modules/passwd-rotation/main.tf
@@ -0,0 +1,36 @@
+data "aws_secretsmanager_secret" "rds-credentials" {
+  name = "production/rds/credentials"
+}
+
+resource "random_password" "rds-password" {
+  length           = 32
+  special          = true
+  override_special = "!#$%^&*()-_=+[]{}|:,.<>?"
+  # Avoid characters that cause issues in connection strings
+  min_lower   = 4
+  min_upper   = 4
+  min_numeric = 4
+  min_special = 2
+}
+
+resource "aws_secretsmanager_secret_version" "rds-credentials" {
+  secret_id = data.aws_secretsmanager_secret.rds-credentials.id
+  secret_string = jsonencode({
+    username            = "memos-user"
+    password = random_password.rds-password.result
+  })
+
+  lifecycle {
+    ignore_changes = [secret_string]
+  }
+}
+
+resource "aws_secretsmanager_secret_rotation" "rds-credentials" {
+  secret_id = data.aws_secretsmanager_secret.rds-credentials.id
+  rotation_lambda_arn = ""
+  
+  rotation_rules {
+    automatically_after_days = 30
+  }
+}
+
