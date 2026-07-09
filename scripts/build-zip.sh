fde12d2d721c37d7cd0689f51d7a8fd8e27fac51
sudo-suadKF <suadfehric@outlook.com>
2026-07-09 10:53:01 +0200

    change: modified the script for building zip files, tested it and it works

--- scripts/build-zip.sh
+++ scripts/build-zip.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+set -e
+
+rm -rf ../infra/build
+mkdir -p ../infra/build
+
+pip3 install --no-cache-dir -r ../infra/src/requirements.txt -t ../infra/build/package
+
+cp ../infra/src/lambda_function.py ../infra/build/package
+
+zip -r ../infra/build/lambda.zip ../infra/build/package
+
+echo "Done! Lambda zip created."
