e9b09e53a4e061209809ac0757537f337c6df697
sudo-suadKF <suadfehric@outlook.com>
2026-07-10 09:53:27 +0200

    change: fixed the iteration issue in subnet ids for vpc endpoint resource

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -99,13 +99,11 @@ resource "aws_route_table_association" "private-rt-to-private-subnets" {
 }
 
 resource "aws_vpc_endpoint" "secretsmanager" {
-  for_each = aws_subnet.private_subnets
-
-  subnet_ids = [each.value.id]
   vpc_id = aws_vpc.my-vpc.id
   service_name = "com.amazonaws.eu-west-2.secretsmanager"
   vpc_endpoint_type = "Interface"
   security_group_ids = [var.vpc-endpoints-sg-id]
+  subnet_ids = [for subnet in aws_subnet.private_subnets : subnet.id]
   private_dns_enabled = true
 
   tags = {
