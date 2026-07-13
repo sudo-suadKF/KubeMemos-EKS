9bcbefdcca57e8d0393e748d26d7bef5143115ba
sudo-suadKF <suadfehric@outlook.com>
2026-07-13 14:04:35 +0200

    change: final script, adding all needed libraries for pg so Lambda secret rotation can work, lambda function tested and confirmed working

--- scripts/build-zip.sh
+++ scripts/build-zip.sh
@@ -2,11 +2,20 @@
 
 set -e
 
+PACKAGE_DIR="../infra/build/package"
+PG_MODULE="$PACKAGE_DIR/pg/_pg.cpython-314-x86_64-linux-gnu.so"
+
 rm -rf ../infra/build/package
 mkdir -p ../infra/build/package
 
 pip3 install --no-cache-dir -r ../infra/src/requirements.txt -t ../infra/build/package
 cp ../infra/src/lambda_function.py ../infra/build
+
+ldd "$PG_MODULE" | awk '/=>/ {print $3} /^\// {print $1}' \
+| while read lib; do
+    echo "Copying $(basename "$lib")"
+    cp -L "$lib" "$PACKAGE_DIR"
+done
 
 cd ../infra/build/package
 zip -r ../lambda.zip .
