815f572aedd9129cd1934d50ced4a7c5bca5b778
sudo-suadKF <suadfehric@outlook.com>
2026-07-22 10:09:49 +0200

    change: improved deploy local script and initialized the local delete script

--- scripts/deploy-local.sh
+++ scripts/deploy-local.sh
@@ -11,18 +11,18 @@ REGION="eu-west-2"
 CLUSTER_NAME="my-eks-cluster"
 
 aws eks update-kubeconfig --region "$REGION" --name "$CLUSTER_NAME"
+echo ""
 
 cd "$WORKING_DIR"
 
 helmfile apply
 echo ""
 echo "Helmfile applied..."
+echo ""
 helm upgrade --install local-app ./my-chart
 echo ""
 echo "Application installed with helm..."
-kubectl apply -f cluster-issuer.yaml
 echo ""
-echo "Cluster issuer applied..."
 
 kubectl apply -f "$GRAFANA_DIR/ingress-http.yaml"
 kubectl apply -f "$GRAFANA_DIR/ingress-https.yaml"
@@ -30,4 +30,14 @@ kubectl apply -f "$PROMETHEUS_DIR/ingress-http.yaml"
 kubectl apply -f "$PROMETHEUS_DIR/ingress-https.yaml"
 kubectl apply -f "$MONITORING_DIR/middleware.yaml"
 echo ""
-echo "Monitoring applied and the setup is done..."
+echo "Monitoring applied..."
+echo ""
+
+kubectl apply -f cluster-issuer.yaml
+echo ""
+echo "Cluster issuer applied and waiting for TLS certificates to become ready..."
+echo ""
+
+kubectl wait --for=condition=ready certificate --all --all-namespaces
+echo ""
+echo "TLS certificates are ready and the setup is done..."
